35feb94f01e1e5ea6566ed3b9d9d7c352d21e8c0
J. Eckard <eckardjf@gmail.com>
2017-01-29 16:24:12 -0500

    add code & test for challenge 3

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,5 +1,6 @@
 (ns cryptopals.core
-  (:import (java.util Base64)))
+  (:import (java.util Base64))
+  (:require [clojure.string :as string]))
 
 (defn hex->bytes [h]
   (->> h
@@ -19,5 +20,44 @@
 (defn hex->base64 [h]
   (bytes->base64 (hex->bytes h)))
 
+(defn string->bytes [^String s]
+  (.getBytes s))
+
+(defn bytes->string [b]
+  (String. b))
+
+
 (defn xor-bytes [b1 b2]
   (byte-array (map bit-xor b1 b2)))
+
+
+(def english-frequencies
+  {\E 0.1249 \T 0.0928 \A 0.0804 \O 0.0764 \I 0.0757 \N 0.0723
+   \S 0.0651 \R 0.0628 \H 0.0505 \L 0.0407 \D 0.0382 \C 0.0334
+   \U 0.0273 \M 0.0251 \F 0.0240 \P 0.0214 \G 0.0187 \W 0.0168
+   \Y 0.0166 \B 0.0148 \V 0.0105 \K 0.0054 \X 0.0023 \J 0.0016
+   \Q 0.0012 \Z 0.0009 \space 0.1832})
+
+(defn square [x]
+  (* x x))
+
+(defn chi-squared [observed expected]
+  (/ (square (- observed expected)) expected))
+
+(defn score-text [s]
+  (reduce + (for [[c n] (frequencies (string/upper-case s))]
+              (chi-squared n (* (count s) (get english-frequencies c 0.01))))))
+
+(defn enumerate-guesses [h]
+  (let [b1 (hex->bytes h)]
+    (for [c (range 32 128)]
+      (let [b2 (byte-array (repeat (count b1) c))
+            result (bytes->string (xor-bytes b1 b2))]
+        {:ch (char c) :score (score-text result) :text result}))))
+
+(defn find-xor-key [h]
+  (->> h
+       enumerate-guesses
+       (sort-by :score)
+       first
+       :ch))
