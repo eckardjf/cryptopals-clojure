7d7c59681b0d834e837c2565049d79e066b762a9
Joe Eckard <joe@backwardpipe.com>
2017-02-05 17:28:29 -0500

    add code & test for challenge 10

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -109,3 +109,25 @@
 (defn pkcs7-pad [n bs]
   (let [p (- n (mod (count bs) n))]
     (byte-array (concat bs (repeat p p)))))
+
+(defn aes-cbc-encrypt [iv k bs]
+  (let [encrypt (partial aes-ecb-encrypt k)]
+    (loop [acc []
+           remaining (partition 16 bs)]
+      (if (empty? remaining)
+        (byte-array (flatten acc))
+        (let [current (first remaining)
+              previous (or (last acc) iv)]
+          (recur (conj acc (encrypt (xor-bytes current previous)))
+                 (rest remaining)))))))
+
+(defn aes-cbc-decrypt [iv k bs]
+  (let [decrypt (partial aes-ecb-decrypt k)]
+    (loop [acc []
+           remaining (reverse (partition 16 bs))]
+      (if (empty? remaining)
+        (byte-array (mapcat seq (reverse acc)))
+        (let [current (first remaining)
+              previous (or (second remaining) iv)]
+          (recur (conj acc (xor-bytes previous (decrypt (byte-array current))))
+                 (rest remaining)))))))
--- test/cryptopals/set_2_test.clj
+++ test/cryptopals/set_2_test.clj
@@ -8,3 +8,11 @@
           result (pkcs7-pad 20 bs)]
       (is (= (count result) 20))
       (is (every? #(= 4 %) (take-last 4 result))))))
+
+(deftest challenge-10-test
+  (testing "Implement CBC mode"
+    (let [k (string->bytes "YELLOW SUBMARINE")
+          iv (repeat 16 0)
+          ciphertext (->> "resources/10.txt" slurp base64->bytes)]
+      (is (-> (bytes->string (aes-cbc-decrypt iv k ciphertext))
+              (.startsWith "I'm back and I'm ringin' the bell"))))))
