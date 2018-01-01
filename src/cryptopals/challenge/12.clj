86b541c65f03255c799d4699e177a6b33c9c36bd
J. Eckard <eckardjf@gmail.com>
2017-12-31 21:14:28 -0500

    add test.check and a crude example

--- src/cryptopals/challenge/12.clj
+++ src/cryptopals/challenge/12.clj
@@ -1,5 +1,9 @@
 (ns cryptopals.challenge.12
   (:require [clojure.test :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [cryptopals.core :refer :all]
             [cryptopals.block :refer [pkcs7-pad aes-ecb-encrypt]]))
 
@@ -12,10 +16,13 @@
 (def unknown-block-size 16)
 (def unknown-key (rand-bytes unknown-block-size))
 
-(defn oracle [bs]
-  (->> (concat bs plaintext)
-       (pkcs7-pad unknown-block-size)
-       (aes-ecb-encrypt unknown-key)))
+(defn oracle-fn
+  ([] (oracle-fn plaintext))
+  ([suffix]
+   (fn [bs]
+     (->> (concat bs suffix)
+          (pkcs7-pad unknown-block-size)
+          (aes-ecb-encrypt unknown-key)))))
 
 (defn determine-block-size [f]
   (let [[a b] (distinct
@@ -45,13 +52,19 @@
     (when (ecb? f block-size)
       (loop [known []]
         (let [next-byte (recover-next-byte f block-size known)]
+          ;; don't like this check - what if there is a valid 1 byte?
           (if (= 1 next-byte)
             (-> known byte-array bytes->string)
             (recur (conj known next-byte))))))))
 
+(defspec sentinel-byte-holds 100
+  (prop/for-all [rand-string (gen/fmap #(apply str %)
+                                       (gen/vector gen/char-alpha 1 100))]
+                (= rand-string (recover-suffix (oracle-fn (string->bytes rand-string))))))
+
 (deftest challenge-12-test
   (testing "Byte-at-a-time ECB decryption (Simple)"
-    (is (= (recover-suffix oracle)
+    (is (= (recover-suffix (oracle-fn))
            (str "Rollin' in my 5.0\n"
                 "With my rag-top down so my hair can blow\n"
                 "The girlies on standby waving just to say hi\n"
