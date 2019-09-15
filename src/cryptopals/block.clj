58c478e49b5a7d653263de7e103fa7bf0fac2855
Joe Eckard <joe@backwardpipe.com>
2019-09-15 13:32:41 -0400

    replace loop recur with reduce

--- src/cryptopals/block.clj
+++ src/cryptopals/block.clj
@@ -18,15 +18,13 @@
     (byte-array (concat bs (repeat p p)))))
 
 (defn aes-cbc-encrypt [iv k bs]
-  (let [encrypt (partial aes-ecb-encrypt k)]
-    (loop [acc []
-           remaining (partition 16 bs)]
-      (if (empty? remaining)
-        (byte-array (mapcat seq acc))
-        (let [current (first remaining)
-              previous (or (last acc) iv)]
-          (recur (conj acc (encrypt (xor-bytes current previous)))
-                 (rest remaining)))))))
+  (->> (reduce (fn [acc block]
+                 (let [previous-block (or (last acc) iv)])
+                 (conj acc (aes-ecb-encrypt k (xor-bytes block previous-block))))
+               []
+               (partition 16 bs))
+       (mapcat seq)
+       byte-array))
 
 (defn aes-cbc-decrypt [iv k bs]
   (let [decrypt (partial aes-ecb-decrypt k)]
